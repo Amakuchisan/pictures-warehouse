ea888afaf4adea0295a273b12e7800a85a12b430
amakuchi <s18t318@stu.kagawa-u.ac.jp>
2018-11-18 01:54:32 +0900

    同じ名前のファイルがアップロードされたとき，別名で保存されるようにした

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='static')
 
+#アップロードの上限サイズを1MBにする
+app.config['MAX_CONTENT_LENGTH'] = 1*1024*1024
+
 UPLOAD_FOLDER = './static'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -12,7 +15,7 @@ def hello():
     return "Hello world"
 
 
-@app.route('/pics')
+# /pics内の処理
 @app.route('/pics', methods=['GET', 'POST'])
 def pictures():
     pictures = glob.glob('static/*')
@@ -23,6 +26,10 @@ def pictures():
         # ファイルがあれば
         if img_file :
             filename = img_file.filename
+            #ここのifに重複しているときに入るようにする
+            if 'static/'+filename in glob.glob('static/*'): 
+                filename = '{0}{1}{2}{3}{4}'.format(filename[:filename.index('.')], '(', glob.glob('static/*').count('static/'+filename), ')', filename[filename.index('.'):])
+                #含まれる個数をカウントして，増やすようにする
             # 保存する
             img_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             pictures = glob.glob('static/*')
