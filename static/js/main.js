24197055471209feb1f6ae28df9ce3e610c230af
amakuchi <s18t318@stu.kagawa-u.ac.jp>
2018-11-22 15:02:17 +0900

    JSの設定を試みようとした, 写真に枠をつけた

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    let del = document.getElementById("delete");
+    del.addEventListener("click", function(){
+        alert('削除するファイルを選んでください');//alert以外に変えたい
+    });
+    
+}
